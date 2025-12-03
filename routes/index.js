4ab13465c539b7e8a5444a73390b60d5ea9a2983
bauerDev <analyticslab.rem@gmail.com>
2025-12-03 13:24:42 +0100

    Subida proyecto examen

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const dataService = require('../data/dataService');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const pokemon = dataService.findAllPokemons();
+  const tipos = dataService.findAllPokemonTypes();
+  res.render('index', { pokemon, tipos });
 });
+
+
+router.get('/pokemon/:id', function(req, res, next) {
+  const pokemon = dataService.findPokemonById(req.params.id);
+  const tipos = dataService.findAllPokemonTypes();
+
+  if (!pokemon) {
+    return res.status(404).send('Pokemon no encontrado');
+  }
+
+  res.render('detalle', { pokemon, tipos });
+});
+
+
+router.get('/tipos/:tipo', (req, res, next) => {
+  const tipo = req.params.tipo;
+  const pokemon = dataService.findAllPokemons().filter(p => p.tipo.includes(tipo));
+  const tipos = dataService.findAllPokemonTypes();
+  res.render('index', { pokemon, tipos });
+});
+
 
 module.exports = router;
